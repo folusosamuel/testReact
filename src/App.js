4709a9e71163aa36df3263e6d9ab0be0606485f7
FolusoSAMUEL <folusosamuel@gmail.com>
2023-07-14 23:04:49 +0100

    Complete todo App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,55 @@ import "./App.css";
 import { Button, Card, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>
+          <b>Add Todo</b>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add new todo"
+        />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+function Todo({ todo, index, completeTodo, deleteTodo }) {
+  return (
+    <div className="todo">
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+        {todo.text}
+      </span>
+      <div>
+        <Button variant="outline-success" onClick={() => completeTodo(index)}>
+          ✓
+        </Button>{" "}
+        <Button variant="outline-danger" onClick={() => deleteTodo(index)}>
+          ✕
+        </Button>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   const [todos, setTodos] = React.useState([
     {
@@ -10,6 +59,47 @@ function App() {
       isDone: false,
     },
   ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  completeTodo={completeTodo}
+                  deleteTodo={deleteTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
